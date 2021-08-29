f6c60de1cf0c14c749cb0e1c9f8be1edc0983c4a
Saurabh Nanda <saurabh@vacationlabs.com>
2021-08-29 14:13:05 +0530

    added bearer auth

---
+++ src/HCloud/Overrides.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+module HCloud.Overrides where
+
+import HCloud.Common(SecurityScheme(..))
+import qualified Data.ByteString as BS
+import Network.HTTP.Client
+import Network.HTTP.Types
+import Network.HTTP.Types.Header (hAuthorization)
+
+newtype BearerAuthenticationSecurityScheme = BearerAuthenticationSecurityScheme { rawToken :: BS.ByteString } deriving (Eq, Show)
+
+instance SecurityScheme BearerAuthenticationSecurityScheme where
+  authenticateRequest BearerAuthenticationSecurityScheme{rawToken} req =
+    req { requestHeaders = (hAuthorization, "Bearer " <> rawToken):(requestHeaders req) }
+
