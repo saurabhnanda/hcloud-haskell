fd8a92476e82cbd6bbb8f0ff2d4e84a868924fb2
Saurabh Nanda <saurabh@vacationlabs.com>
2021-08-29 22:52:04 +0530

    removed commented code

--- src/HCloud/Overrides.hs
+++ src/HCloud/Overrides.hs
@@ -8,11 +8,5 @@ import Network.HTTP.Client
 import Network.HTTP.Types
 import Network.HTTP.Types.Header (hAuthorization)
 
--- newtype BearerAuthenticationSecurityScheme = BearerAuthenticationSecurityScheme { rawToken :: BS.ByteString } deriving (Eq, Show)
-
--- instance SecurityScheme BearerAuthenticationSecurityScheme where
---   authenticateRequest BearerAuthenticationSecurityScheme{rawToken} req =
---     req { requestHeaders = (hAuthorization, "Bearer " <> rawToken):(requestHeaders req) }
-
 bearerAuthenticationSecurityScheme :: BS.ByteString -> Request -> Request
 bearerAuthenticationSecurityScheme rawToken req = req { requestHeaders = (hAuthorization, "Bearer " <> rawToken):(requestHeaders req) }
